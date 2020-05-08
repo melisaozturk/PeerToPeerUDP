3087bf75c93eb8f6a87c3f36ded80fffee80375e
Melisa Öztürk <mlsaoztrk@icloud.com>
2020-05-08 21:27:45 +0300

    udp

--- Documents/Projelerim/PearToPearNetwork/PearToPearNetwork/Network/PeerConnection.swift
+++ Documents/Projelerim/PearToPearNetwork/PearToPearNetwork/Network/PeerConnection.swift
@@ -20,40 +20,52 @@ protocol PeerConnectionDelegate: class {
 class PeerConnection {
     
     var connection: NWConnection?
-    var hostUDP: NWEndpoint.Host = "iperf.volia.net"
-    var portUDP: NWEndpoint.Port = 5201
     let initiatedConnection: Bool
     weak var delegate: PeerConnectionDelegate?
+    var hostUDP: NWEndpoint.Host = "192.168.4.1"
+    var portUDP: NWEndpoint.Port = 5555
     
+    // Create an outbound connection when the user initiates a video.
     init(endpoint: NWEndpoint, interface: NWInterface?, delegate: PeerConnectionDelegate) {
         self.delegate = delegate
         self.initiatedConnection = true
 
-        connect()
+        startConnection()
+        
     }
     
-    // Handle an inbound connection when the user receives a game request.
+    // Handle an inbound connection when the user receives a video request.
     init(connection: NWConnection, delegate: PeerConnectionDelegate) {
         self.delegate = delegate
         self.connection = connection
         self.initiatedConnection = false
 
-        connect()
+        startConnection()
     }
     
-    func connect() {
+    // Handle the user exiting the video.
+    func cancel() {
+        if let connection = self.connection {
+            connection.cancel()
+            self.connection = nil
+        }
+    }
+
+    // Handle starting the peer-to-peer connection for both inbound and outbound connections.
+    func startConnection() {
         // Hack to wait until everything is set up
         var x = 0
         while(x<1000000000) {
             x+=1
         }
+//        connection = NWConnection(host: "192.168.4.1", port: 4210, using: .udp)
         connectToUDP(hostUDP,portUDP)
     }
     
     func connectToUDP(_ hostUDP: NWEndpoint.Host, _ portUDP: NWEndpoint.Port) {
         // Transmited message:
         let messageToUDP = "Test message"
-        
+       
         self.connection = NWConnection(host: hostUDP, port: portUDP, using: .udp)
         
         self.connection?.stateUpdateHandler = { (newState) in
@@ -62,13 +74,27 @@ class PeerConnection {
             case .ready:
                 print("State: Ready\n")
                 self.sendUDP(messageToUDP)
+//                self.sendUDP(data)
                 self.receiveUDP()
+                // Notify your delegate that the connection is ready.
+                if let delegate = self.delegate {
+                    delegate.connectionReady()
+                }
             case .setup:
                 print("State: Setup\n")
             case .cancelled:
                 print("State: Cancelled\n")
             case .preparing:
                 print("State: Preparing\n")
+            case .failed:
+                // Cancel the connection upon a failure.
+                guard let connection = self.connection else {return}
+                connection.cancel()
+
+                // Notify your delegate that the connection failed.
+                if let delegate = self.delegate {
+                    delegate.connectionFailed()
+                }
             default:
                 print("ERROR! State not defined!\n")
             }
@@ -77,6 +103,7 @@ class PeerConnection {
         self.connection?.start(queue: .global())
     }
     
+    // Handle sending a "video message".
     func sendUDP(_ content: Data) {
         self.connection?.send(content: content, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
             if (NWError == nil) {
@@ -87,6 +114,7 @@ class PeerConnection {
         })))
     }
     
+    // Handle sending a "string message".
     func sendUDP(_ content: String) {
         let contentToSendUDP = content.data(using: String.Encoding.utf8)
         self.connection?.send(content: contentToSendUDP, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
@@ -98,15 +126,28 @@ class PeerConnection {
         })))
     }
     
+    // Receive a message, deliver it to your delegate, and continue receiving more messages.
     func receiveUDP() {
-        self.connection?.receiveMessage { (data, context, isComplete, error) in
+        guard let connection = connection else {
+            return
+        }
+        connection.receiveMessage { (data, context, isComplete, error) in
             if (isComplete) {
                 print("Receive is complete")
                 if (data != nil) {
-                    let backToString = String(decoding: data!, as: UTF8.self)
-                    print("Received message: \(backToString)")
-                } else {
-                    print("Data == nil")
+                    
+                    // Extract your message type from the received context.
+                    if let videoMessage = context?.protocolMetadata(definition: VideoProtocol.definition) as? NWProtocolFramer.Message {
+                        self.delegate?.receivedMessage(content: data, message: videoMessage)
+                    }
+                    
+                    //                    let backToString = String(decoding: data!, as: UTF8.self)
+                    //                    print("Received message: \(backToString)")
+                    
+                    if error == nil {
+                        // Continue to receive more messages until you receive and error.
+                        self.receiveUDP()
+                    }
                 }
             }
         }
--- Documents/Projelerim/PearToPearNetwork/PearToPearNetwork/Network/PeerListener.swift
+++ Documents/Projelerim/PearToPearNetwork/PearToPearNetwork/Network/PeerListener.swift
@@ -11,74 +11,187 @@ import Network
 var sharedListener: PeerListener?
 
 class PeerListener {
-    
     var udpListener:NWListener?
-    weak var delegate: PeerConnectionDelegate?
-    var listener: NWListener?
+    var backgroundQueueUdpListener   = DispatchQueue(label: "udp-lis.bg.queue", attributes: [])
+    var backgroundQueueUdpConnection = DispatchQueue(label: "udp-con.bg.queue", attributes: [])
     var name: String
+    weak var delegate: PeerConnectionDelegate?
     
-    // Create a listener with a name to advertise, a passcode for authentication,
-    // and a delegate to handle inbound connections.
-    init(name: String, delegate: PeerConnectionDelegate) {
+    init(name:String, delegate: PeerConnectionDelegate) {
         self.delegate = delegate
         self.name = name
         startListening()
     }
     
-    // Start listening and advertising.
-    func startListening() {
+    
+    private func startListening() {
+        
         do {
             guard self.udpListener == nil else {
                 print("Already listening. Not starting again")
                 return
             }
             
-            // Create the listener object.
             self.udpListener = try NWListener(using: .udp, on: 55555)
-            
+            self.udpListener?.stateUpdateHandler = { (listenerState) in
+                print(" NWListener Handler called")
+                switch listenerState {
+                case .setup:
+                    print("Listener: Setup")
+                case .waiting(let error):
+                    print("Listener: Waiting \(error)")
+                case .ready:
+                    print("Listener:  Ready and listens on port: \(self.udpListener?.port?.debugDescription ?? "-")")
+                case .failed(let error):
+                    print("Listener: Failed \(error)")
+                case .cancelled:
+                    print("Listener:    Cancelled by myOffButton")
+                default:
+                    break
+                    
+                }
+            }
             // Set the service to advertise.
             self.udpListener!.service = NWListener.Service(name: self.name, type: "_tictactoe._tcp")
             
-            self.udpListener!.stateUpdateHandler = { newState in
-                switch newState {
-                case .ready:
-                    print("Listener ready on \(String(describing: self.udpListener!.port))")
-                case .failed(let error):
-                    // If the listener fails, re-start.
-                    print("Listener failed with \(error), restarting")
-                    self.udpListener!.cancel()
-                    self.startListening()
-                default:
-                    break
-                }
-            }
-            
-            self.udpListener!.newConnectionHandler = { newConnection in
-                if let delegate = self.delegate {
-                    if sharedConnection == nil {
-                        // Accept a new connection.
-                        sharedConnection = PeerConnection(connection: newConnection, delegate: delegate)
-                    } else {
-                        // If a game is already in progress, reject it.
-                        newConnection.cancel()
+            // Start listening, and request updates on the queue.
+            self.udpListener?.start(queue: backgroundQueueUdpListener)
+            self.udpListener?.newConnectionHandler = { (incomingUdpConnection) in
+                print(" NWConnection Handler called ")
+                incomingUdpConnection.stateUpdateHandler = { (udpConnectionState) in
+                    if let delegate = self.delegate {
+
+                    switch udpConnectionState {
+                    case .setup:
+                        print("Connection:  setup")
+                    case .waiting(let error):
+                        print("Connection:  waiting: \(error)")
+                    case .ready:
+                        print("Connection:  ready")
+                        if sharedConnection == nil {
+                            // Accept a new connection.
+                            sharedConnection = PeerConnection(connection: incomingUdpConnection, delegate: delegate)
+//                            self.processData(incomingUdpConnection)
+                            
+                        } else {
+                            // If a video is already in progress, reject it.
+                            incomingUdpConnection.cancel()
+                        }
+                    case .failed(let error):
+                        print("Connection:  failed: \(error)")
+                    case .cancelled:
+                        print("Connection:    cancelled")
+                    default:
+                        break
+                        }
                     }
                 }
+                incomingUdpConnection.start(queue: self.backgroundQueueUdpConnection)
             }
             
-            // Start listening, and request updates on the main queue.
-            self.udpListener!.start(queue: .main)
         } catch {
             print("Failed to create listener")
             abort()
         }
     }
-    
-    // If the user changes their name, update the advertised name.
+    //    @IBAction func myOffButton(_ sender: Any) {
+    //        udpListener?.cancel()
+    //    }
+
+//    func processData(_ incomingUdpConnection :NWConnection) {
+//
+//        incomingUdpConnection.receiveMessage(completion: {(data, context, isComplete, error) in
+//
+//            if let data = data, !data.isEmpty {
+//                if let string = String(data: data, encoding: .ascii) {
+//                    print ("DATA       = \(string)")
+//                }
+//            }
+//            //print ("context    = \(context)")
+//            print ("isComplete = \(isComplete)")
+//            //print ("error      = \(error)")
+//
+//            self.processData(incomingUdpConnection)
+//        })
+//
+//    }
+//
     func resetName(_ name: String) {
         self.name = name
-        if let listener = listener {
+        if let listener = udpListener {
             // Reset the service to advertise.
             listener.service = NWListener.Service(name: self.name, type: "_tictactoe._tcp")
         }
     }
 }
+//
+//    var udpListener:NWListener?
+//    weak var delegate: PeerConnectionDelegate?
+//    var listener: NWListener?
+//    var name: String
+//
+//    // Create a listener with a name to advertise,
+//    // delegate to handle inbound connections.
+//    init(name: String, delegate: PeerConnectionDelegate) {
+//        self.delegate = delegate
+//        self.name = name
+//        startListening()
+//    }
+//
+//    // Start listening and advertising.
+//    func startListening() {
+//        do {
+//            guard self.udpListener == nil else {
+//                print("Already listening. Not starting again")
+//                return
+//            }
+//
+//            // Create the listener object.
+//            self.udpListener = try NWListener(using: .udp, on: 55555)
+//
+//            // Set the service to advertise.
+//            self.udpListener!.service = NWListener.Service(name: self.name, type: "_tictactoe._tcp")
+//
+//            self.udpListener!.stateUpdateHandler = { newState in
+//                switch newState {
+//                case .ready:
+//                    print("Listener ready on \(String(describing: self.udpListener!.port))")
+//                case .failed(let error):
+//                    // If the listener fails, re-start.
+//                    print("Listener failed with \(error), restarting")
+//                    self.udpListener!.cancel()
+//                    self.startListening()
+//                default:
+//                    break
+//                }
+//            }
+//
+//            self.udpListener!.newConnectionHandler = { newConnection in
+//                if let delegate = self.delegate {
+//                    if sharedConnection == nil {
+//                        // Accept a new connection.
+//                        sharedConnection = PeerConnection(connection: newConnection, delegate: delegate)
+//                    } else {
+//                        // If a game is already in progress, reject it.
+//                        newConnection.cancel()
+//                    }
+//                }
+//            }
+//
+//            // Start listening, and request updates on the main queue.
+//            self.udpListener!.start(queue: .main)
+//        } catch {
+//            print("Failed to create listener")
+//            abort()
+//        }
+//    }
+//
+//    // If the user changes their name, update the advertised name.
+//    func resetName(_ name: String) {
+//        self.name = name
+//        if let listener = listener {
+//            // Reset the service to advertise.
+//            listener.service = NWListener.Service(name: self.name, type: "_tictactoe._tcp")
+//        }
+//    }
+//}
